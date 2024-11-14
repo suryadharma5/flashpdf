2d04fd329a9cade36cb1600217677518625be492
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-11-14 21:20:41 +0700

    feat(zod schema): add zod schema for login and register

---
+++ lib/types/auth.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+export const registerSchema = z
+  .object({
+    email: z.string().email("Please enter a valid email address"),
+    username: z
+      .string()
+      .min(3, "Username must be at least 3 characters long")
+      .max(20, "Username must be no longer than 20 characters"),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .max(30, "Password must be no longer than 30 characters"),
+    confirmPassword: z
+      .string()
+      .min(8, "Confirm password must be at least 8 characters long")
+      .max(30, "Confirm password must be no longer than 30 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password don't match",
+    path: ["confirmPassword"],
+  });
+
+export const loginSchema = z.object({
+  email: z.string().email("Please enter a valid email address"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(30, "Password must be no longer than 30 characters"),
+});
+
+export type TRegisterSchema = z.infer<typeof registerSchema>;
+export type TLoginSchema = z.infer<typeof loginSchema>;
