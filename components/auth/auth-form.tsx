bac06b74fe07361f63dc062689e474d834144b44
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-11-14 21:21:17 +0700

    feat(component): add auth form component using react hook form

---
+++ components/auth/auth-form.tsx
@@ -0,0 +1,229 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { loginSchema, registerSchema } from "@/lib/types/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+type RegisterUserProps = {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+type LoginUserProps = {
+  email: string;
+  password: string;
+};
+
+type AuthFormProps = {
+  type: "login" | "register";
+};
+
+export default function AuthForm({ type }: AuthFormProps) {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const schema = type === "login" ? loginSchema : registerSchema;
+  type AuthenticationType = z.infer<typeof schema>;
+
+  const form = useForm<AuthenticationType>({
+    resolver: zodResolver(schema),
+    defaultValues:
+      type === "login"
+        ? {
+            email: "",
+            password: "",
+          }
+        : {
+            email: "",
+            username: "",
+            password: "",
+            confirmPassword: "",
+          },
+  });
+
+  const handleSubmit = (data: AuthenticationType) => {
+    try {
+      console.log({ data });
+      setShowPassword(false);
+      setShowConfirmPassword(false);
+      form.reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleGoogleLogin = () => {
+    // Handle Google login logic here
+    console.log("Google login attempted");
+  };
+
+  return (
+    <div className="mt-6">
+      <Form {...form}>
+        <form className="space-y-6" onSubmit={form.handleSubmit(handleSubmit)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <div className="mt-1">
+                    <Input
+                      {...field}
+                      onChange={(e) => field.onChange(e.target.value)}
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {type === "register" && (
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <div className="mt-1">
+                      <Input
+                        {...field}
+                        onChange={(e) => field.onChange(e.target.value)}
+                        id="username"
+                        name="username"
+                        type="text"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )}
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <div className="relative mt-1">
+                    <Input
+                      {...field}
+                      onChange={(e) => field.onChange(e.target.value)}
+                      id="password"
+                      name="password"
+                      type={showPassword ? "text" : "password"}
+                      required
+                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+                    />
+                    <button
+                      type="button"
+                      className="absolute inset-y-0 right-0 flex items-center pr-3"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? (
+                        <EyeIcon
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      ) : (
+                        <EyeOffIcon
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      )}
+                    </button>
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {type === "register" && (
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <div className="relative mt-1">
+                      <Input
+                        {...field}
+                        onChange={(e) => field.onChange(e.target.value)}
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        type={showConfirmPassword ? "text" : "password"}
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+                      />
+                      <button
+                        type="button"
+                        className="absolute inset-y-0 right-0 flex items-center pr-3"
+                        onClick={() =>
+                          setShowConfirmPassword(!showConfirmPassword)
+                        }
+                      >
+                        {showConfirmPassword ? (
+                          <EyeIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                        ) : (
+                          <EyeOffIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                        )}
+                      </button>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )}
+
+          <div>
+            <Button
+              type="submit"
+              disabled={form.formState.isSubmitting}
+              className="flex w-full justify-center rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2"
+            >
+              {type === "login" ? "Sign In" : "Create Account"}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
