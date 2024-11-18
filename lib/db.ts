8fe4cf767d71d165f458fbc6e652e2ce74387f97
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-11-18 11:33:55 +0700

    feat(database): add maxwait and timeout options in prisma instance

--- lib/db.ts
+++ lib/db.ts
@@ -4,6 +4,13 @@ declare global {
   var prisma: PrismaClient;
 }
 
-export const prismaClient = globalThis.prisma || new PrismaClient();
+export const prismaClient =
+  globalThis.prisma ||
+  new PrismaClient({
+    transactionOptions: {
+      maxWait: 5000,
+      timeout: 10000,
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismaClient;
