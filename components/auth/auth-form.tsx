6d7d72c0ee4f7bbfdc7274859c06dc78f10170f5
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-11-30 00:38:32 +0700

    refactor(auth): refactor login and register function to use custom hook

--- components/auth/auth-form.tsx
+++ components/auth/auth-form.tsx
@@ -10,18 +10,24 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useLogin } from "@/hooks/useLogin";
+import { useRegister } from "@/hooks/useRegister";
 import { REDIRECT_ROUTE } from "@/lib/auth/oauth/oauth";
-import { axiosInstance } from "@/lib/axios";
-import { loginSchema, registerSchema, TLoginSchema } from "@/lib/types/auth";
+import {
+  loginSchema,
+  registerSchema,
+  TLoginSchema,
+  TRegisterSchema,
+} from "@/lib/types/auth";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { EyeIcon, EyeOffIcon } from "lucide-react";
-import { signIn } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { toast } from "sonner";
+import { ClipLoader } from "react-spinners";
 import { z } from "zod";
 import { FormError } from "./form-error";
+import { FormSuccess } from "./form-success";
 
 type AuthFormProps = {
   type: "login" | "register";
@@ -33,11 +39,16 @@ type ErrorAuthenticationProps = {
 };
 
 export default function AuthForm({ type }: AuthFormProps) {
+  const router = useRouter();
+
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [errorLogin, setErrorLogin] = useState<ErrorAuthenticationProps | null>(
     null,
   );
+  const [errorRegister, setErrorRegister] =
+    useState<ErrorAuthenticationProps | null>(null);
+  const [success, setSuccess] = useState<string | null>(null);
 
   const schema = type === "login" ? loginSchema : registerSchema;
   type AuthenticationType = z.infer<typeof schema>;
@@ -58,56 +69,47 @@ export default function AuthForm({ type }: AuthFormProps) {
           },
   });
 
+  const { mutate: login, isPending: loginPending } = useLogin(
+    () => {
+      form.reset();
+      router.push(REDIRECT_ROUTE);
+    },
+    (error: { message: string; status: number }) => {
+      setErrorLogin(error);
+      form.resetField("password");
+    },
+  );
+
+  const { mutate: register, isPending: registerPending } = useRegister(
+    () => {
+      form.reset();
+      setSuccess("Confirmation email sent!");
+      setErrorRegister(null);
+    },
+    (error: { message: string; status: number }) => {
+      setErrorRegister(error);
+      setSuccess(null);
+      form.resetField("password");
+      form.resetField("confirmPassword");
+    },
+  );
+
   const handleLogin = async (data: TLoginSchema) => {
-    await axiosInstance
-      .post("/api/auth/login", {
-        ...data,
-      })
-      .then(() => {
-        console.log("success");
-        setErrorLogin(null);
-      })
-      .catch((e) => {
-        setErrorLogin(e.response?.data);
+    setErrorLogin(null);
+    login(data);
+  };
 
-        if (e.response?.data?.status == 401) {
-          toast.info("Email verification sent", {
-            description:
-              "Please verify your email first to continue using flashAI",
-            duration: 5000,
-          });
-        }
-      });
-
-    if (errorLogin === null) {
-      await signIn("credentials", {
-        email: data.email,
-        password: data.password,
-        redirect: false,
-      }).then((data) => {
-        if (data?.error) {
-          console.log(data);
-        } else {
-          form.reset();
-
-          setShowPassword(false);
-          setShowConfirmPassword(false);
-
-          toast.success("Login Success!");
-
-          redirect(REDIRECT_ROUTE);
-        }
-      });
-    }
-
-    form.resetField("password");
+  const handleRegister = async (data: TRegisterSchema) => {
+    setErrorRegister(null);
+    setSuccess(null);
+    register(data);
   };
 
   const handleSubmit = async (data: AuthenticationType) => {
     if (type === "login") {
       await handleLogin(data);
     } else {
-      console.log("handle register");
+      await handleRegister(data as TRegisterSchema);
     }
   };
 
@@ -253,17 +255,31 @@ export default function AuthForm({ type }: AuthFormProps) {
           )}
 
           <FormError
-            message={errorLogin?.message}
+            message={errorLogin?.message || errorRegister?.message}
             type={errorLogin?.status === 401 ? "warning" : "error"}
           />
+
+          <FormSuccess message={success} />
 
           <div>
             <Button
               type="submit"
-              disabled={form.formState.isSubmitting}
+              disabled={loginPending || registerPending}
               className="flex w-full justify-center rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-800"
             >
-              {type === "login" ? "Sign In" : "Create Account"}
+              <ClipLoader
+                color="white"
+                size={15}
+                className="mr-1"
+                loading={loginPending || registerPending}
+              />
+              <p>
+                {loginPending || registerPending
+                  ? "Loading..."
+                  : type === "login"
+                    ? "Sign In"
+                    : "Create Account"}
+              </p>
             </Button>
           </div>
         </form>
