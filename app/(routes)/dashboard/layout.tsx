fcbc40667cf4be151b5b417866ab3d4296ed8a2d
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-12-16 14:22:50 +0700

    style(layout): change layout size

--- app/(routes)/dashboard/layout.tsx
+++ app/(routes)/dashboard/layout.tsx
@@ -20,7 +20,9 @@ export default async function DashboardLayout({
             <div className="flex flex-1 flex-col overflow-hidden">
               <Navbar />
               <main className="flex-1 overflow-y-auto overflow-x-hidden">
-                <div className="mx-auto px-4 py-8 xl:max-w-5xl">{children}</div>
+                <div className="mx-auto flex justify-center px-4 py-8 xl:max-w-6xl">
+                  {children}
+                </div>
               </main>
             </div>
           </SidebarInset>
