5bacb8502bb807dbe1913fae4c58ffaefe7f495e
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-12-19 10:21:02 +0700

    refactor(library): change library page ui

--- app/(routes)/dashboard/material/library/page.tsx
+++ app/(routes)/dashboard/material/library/page.tsx
@@ -6,7 +6,6 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
@@ -18,19 +17,24 @@ import { useQuery } from "@tanstack/react-query";
 import { formatDistanceToNowStrict } from "date-fns";
 import {
   AlertCircle,
+  CircleAlert,
+  CircleCheckBig,
   Clock9,
-  FileText,
-  NotebookPen,
   Pencil,
   Trash2,
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
+type HistoryProps = {
+  type: string;
+};
+
 type DocumentProps = {
   id: string;
   createdAt: string;
   title: string;
+  History: HistoryProps[];
 };
 
 export default function Page() {
@@ -53,86 +57,125 @@ export default function Page() {
   const documents: DocumentProps[] = data.data;
 
   return (
-    <>
-      {documents.length > 0 ? (
-        documents.map((data, idx) => (
-          <div
-            className="grid w-full gap-4 md:grid-cols-2 lg:grid-cols-3"
-            key={idx}
-          >
-            <Card key={data.id}>
+    <div className="container mx-auto max-w-7xl p-4 sm:p-6">
+      <h1 className="mb-6 text-3xl font-bold">Your Documents</h1>
+      <div className="grid w-full gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {documents.length > 0 ? (
+          documents.map((data, idx) => (
+            <Card
+              key={data.id}
+              className="flex w-full flex-col transition-all duration-300 hover:shadow-lg"
+            >
               <CardHeader>
-                <div className="flex items-start justify-between">
-                  <div>
-                    <CardTitle>
-                      {data.title.replace(
-                        data.title.charAt(0),
-                        data.title.charAt(0).toUpperCase(),
-                      )}
+                <div className="flex max-w-full items-start justify-between">
+                  <div className="max-w-full flex-1">
+                    <CardTitle className="overflow-hidden truncate">
+                      {data.title
+                        .replace(
+                          data.title.charAt(0),
+                          data.title.charAt(0).toUpperCase(),
+                        )
+                        .slice(0, 20)}
+
+                      {data.title.length > 20 ? "..." : ""}
                     </CardTitle>
-                    <CardDescription>category</CardDescription>
+                    <CardDescription>
+                      <div className="mt-2 flex items-center gap-2">
+                        <Clock9 size={15} />
+                        <p className="text-xs">
+                          {formatDistanceToNowStrict(data.createdAt)} ago
+                        </p>
+                      </div>
+                    </CardDescription>
                   </div>
-                  <Badge
-                    variant="default"
-                    className="bg-yellow-100 text-yellow-500"
-                  >
-                    <>
+                  {data.History.length > 0 ? (
+                    <Badge
+                      variant="default"
+                      className="ml-1 bg-green-100 text-green-500"
+                    >
+                      <CircleCheckBig className="mr-1 h-4 w-4" />
+                    </Badge>
+                  ) : (
+                    <Badge
+                      variant="default"
+                      className="ml-1 bg-yellow-100 text-yellow-500"
+                    >
                       <AlertCircle className="mr-1 h-4 w-4" />
-                      Unfinished Pretest
-                    </>
-                  </Badge>
+                    </Badge>
+                  )}
                 </div>
               </CardHeader>
-              <CardContent className="flex flex-col justify-center gap-2 text-muted-foreground">
-                <div className="flex items-center gap-2">
-                  <Clock9 size={15} />
-                  <p className="text-xs">
-                    {formatDistanceToNowStrict(data.createdAt)} ago
-                  </p>
-                </div>
-              </CardContent>
-              <CardFooter className="flex flex-col">
-                <div className="flex w-full justify-between">
-                  <Button variant="outline" size="sm">
-                    <NotebookPen className="h-4 w-4" />
-                    Review
-                  </Button>
-                  <Button variant="outline" size="sm">
-                    <Pencil className="h-4 w-4" />
-                    Edit
-                  </Button>
-                  <Button variant="outline" size="sm">
-                    <Trash2 className="h-4 w-4" />
-                    Delete
-                  </Button>
-                </div>
+              <CardFooter className="flex flex-wrap gap-2">
+                {data.History.length > 0 ? (
+                  <div className="mb-2 flex w-full gap-2">
+                    <Button
+                      variant="outline"
+                      size="sm"
+                      className="flex-1 hover:border-yellow-500 hover:bg-white"
+                    >
+                      <Pencil className="mr-2 h-4 w-4" />
+                      Edit
+                    </Button>
+                    <Button
+                      variant="outline"
+                      size="sm"
+                      className="flex-1 hover:border-destructive hover:bg-white"
+                    >
+                      <Trash2 className="mr-2 h-4 w-4" />
+                      Delete
+                    </Button>
+                  </div>
+                ) : (
+                  <div className="mb-2 flex rounded-md bg-yellow-100 px-3 py-1 text-yellow-800 dark:bg-yellow-900">
+                    <CircleAlert className="mr-2 h-4 w-4" />
+                    <p className="text-xs dark:text-yellow-200">
+                      Complete the pretest to unlock full flashcard features.
+                    </p>
+                  </div>
+                )}
 
                 <Link
-                  href={`/dashboard/material/library/document/${data.id}/pretest`}
-                  className="mt-4 w-full"
+                  className="w-full"
+                  href={`http://localhost:3000/dashboard/material/library/document/${data.id}/flashcard`}
                 >
-                  <Button variant="default" size="sm" className="w-full">
-                    <FileText className="h-4 w-4" />
-                    Take Pre-test
+                  <Button
+                    size="sm"
+                    className="w-full"
+                    disabled={data.History.length <= 0}
+                  >
+                    View Flashcards
+                  </Button>
+                </Link>
+                <Link
+                  href={`/dashboard/material/library/document/${data.id}/pretest`}
+                  className="w-full"
+                >
+                  <Button variant="outline" size="sm" className="w-full">
+                    Take {data.History[idx] ? "Post-test" : "Pre-test"}
                   </Button>
                 </Link>
               </CardFooter>
             </Card>
+          ))
+        ) : (
+          <div className="flex h-[80vh] w-full flex-col items-center justify-center">
+            <Image
+              src={EmptyImage}
+              alt="empty image"
+              width={250}
+              height={250}
+            />
+            <p className="mt-12 text-center text-2xl font-light">
+              You don't have any flashcard yet
+            </p>
+            <Link href="/dashboard/material/create">
+              <Button className="mt-4" type="button">
+                Create now
+              </Button>
+            </Link>
           </div>
-        ))
-      ) : (
-        <div className="flex h-[80vh] w-full flex-col items-center justify-center">
-          <Image src={EmptyImage} alt="empty image" width={250} height={250} />
-          <p className="mt-12 text-center text-2xl font-light">
-            You don't have any flashcard yet
-          </p>
-          <Link href="/dashboard/material/create">
-            <Button className="mt-4" type="button">
-              Create now
-            </Button>
-          </Link>
-        </div>
-      )}
-    </>
+        )}
+      </div>
+    </div>
   );
 }
