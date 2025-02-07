1ea5cfec9dc00ccc93e81a2e881293809dae43ee
suryadharma5 <suryadharmasetiawan03@gmail.com>
2025-02-07 15:03:24 +0700

    feat(zod schema): add zod schema for forum

---
+++ lib/types/forum.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const forumSchema = z.object({
+  title: z.string().min(1),
+  description: z
+    .string()
+    .min(1)
+    .max(100, "Description must be no longer than 30 characters"),
+  documentId: z.string().min(1),
+});
+
+export const createForumSchema = z.object({
+  title: z.string().min(1),
+  description: z
+    .string()
+    .min(1)
+    .max(100, "Description must be no longer than 30 characters"),
+  documentId: z.string().min(1),
+  userId: z.string().min(1),
+});
+
+export const deleteForumSchema = z.object({
+  documentId: z.string().min(1),
+  forumId: z.string().min(1),
+});
+
+export type TForumSchema = z.infer<typeof forumSchema>;
+export type TCreateForumSchema = z.infer<typeof createForumSchema>;
+export type TDeleteForumSchema = z.infer<typeof deleteForumSchema>;
