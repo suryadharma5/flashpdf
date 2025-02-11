05339dc8587e2f4058ef56a8c0f8b2203c88d094
suryadharma5 <suryadharmasetiawan03@gmail.com>
2025-02-12 00:01:29 +0700

    feat(forum): helper function for forum data

--- lib/repository/forum/forumRepository.ts
+++ lib/repository/forum/forumRepository.ts
@@ -19,6 +19,45 @@ export const createForum = async (
   return forum;
 };
 
+export const getAllForum = async (userId: string, tx?: PrismaTransaction) => {
+  const prismaTx = tx || prismaClient;
+
+  const forums = await prismaTx.forum.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    select: {
+      id: true,
+      createdAt: true,
+      title: true,
+      description: true,
+      documentId: true,
+      totalLike: true,
+      user: {
+        select: {
+          username: true,
+          image: true,
+        },
+      },
+      likes: {
+        where: { userId },
+        select: { id: true },
+      },
+      comments: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  });
+
+  if (!forums) {
+    return null;
+  }
+
+  return forums;
+};
+
 export const findForumById = async (
   forumId: string,
   tx?: PrismaTransaction,
@@ -55,7 +94,113 @@ export const deleteForum = async (
     });
 
     return { success: true };
-  } catch (_) {
+  } catch (e) {
     return { success: false };
   }
+};
+
+export const getLikeById = async (forumId: string, userId: string) => {
+  const like = await prismaClient.forumLike.findFirst({
+    where: {
+      forumId: forumId,
+      userId: userId,
+    },
+  });
+
+  if (!like) {
+    return null;
+  }
+
+  return like;
+};
+
+export const createLike = async (forumId: string, userId: string) => {
+  await prismaClient.forumLike.create({
+    data: {
+      forumId,
+      userId,
+    },
+  });
+
+  const like = await prismaClient.forum.update({
+    where: {
+      id: forumId,
+    },
+    data: {
+      totalLike: {
+        increment: 1,
+      },
+    },
+  });
+
+  return like.totalLike;
+};
+
+export const deleteLike = async (id: string, forumId: string) => {
+  await prismaClient.forumLike.delete({
+    where: {
+      id,
+    },
+  });
+
+  const like = await prismaClient.forum.update({
+    where: {
+      id: forumId,
+    },
+    data: {
+      totalLike: {
+        decrement: 1,
+      },
+    },
+  });
+
+  return like.totalLike;
+};
+
+export const createComment = async (
+  forumId: string,
+  userId: string,
+  comment: string,
+) => {
+  const newComment = await prismaClient.comment.create({
+    data: {
+      forumId,
+      userId,
+      comment,
+    },
+  });
+
+  if (!newComment) {
+    return null;
+  }
+
+  return newComment;
+};
+
+export const getCommentsById = async (forumId: string) => {
+  const comments = await prismaClient.comment.findMany({
+    where: {
+      forumId,
+    },
+    select: {
+      id: true,
+      comment: true,
+      createdAt: true,
+      user: {
+        select: {
+          username: true,
+          image: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  if (!comments) {
+    return null;
+  }
+
+  return comments;
 };
