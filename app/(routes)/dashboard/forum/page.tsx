886e36d3aab3dab2a717bf08a9e91f33ff72d4c2
suryadharma5 <suryadharmasetiawan03@gmail.com>
2025-02-12 00:03:13 +0700

    feat(forum): add forum UI

---
+++ app/(routes)/dashboard/forum/page.tsx
@@ -0,0 +1,200 @@
+"use client";
+
+import { Empty } from "@/components/dashboard/empty";
+import ErrorPage from "@/components/dashboard/error";
+import Comment, { ForumProps } from "@/components/dashboard/forum/comment";
+import { LoadingPage } from "@/components/dashboard/loading";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { axiosInstance } from "@/lib/axios";
+import EmptyImage from "@/public/not-found-page.svg";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { formatDistanceToNowStrict } from "date-fns";
+import { ArrowUpRight, Heart, MessageCircle } from "lucide-react";
+import Link from "next/link";
+
+export default function ForumPage() {
+  const queryClient = useQueryClient();
+
+  const {
+    data: posts,
+    isError,
+    isPending,
+  } = useQuery({
+    queryKey: ["fetchForum"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/api/forum");
+      return res.data.data as ForumProps[];
+    },
+  });
+
+  const likeMutation = useMutation({
+    mutationFn: async (forumId: string) => {
+      const res = await axiosInstance.patch(`/api/forum/${forumId}/like`);
+
+      return res.data;
+    },
+    onMutate: async (forumId) => {
+      await queryClient.cancelQueries({ queryKey: ["fetchForum"] });
+
+      const previousData = queryClient.getQueryData<ForumProps[]>([
+        "fetchForum",
+      ]);
+
+      queryClient.setQueryData(
+        ["fetchForum"],
+        (oldForums: ForumProps[] | undefined) => {
+          if (!oldForums) return [];
+          return oldForums.map((forum) =>
+            forum.id === forumId
+              ? {
+                  ...forum,
+                  isLiked: !forum.isLiked,
+                  totalLike: forum.isLiked
+                    ? forum.totalLike - 1
+                    : forum.totalLike + 1,
+                }
+              : forum,
+          );
+        },
+      );
+
+      return { previousData };
+    },
+    onSuccess: (data, forumId) => {
+      queryClient.setQueryData(
+        ["fetchForum"],
+        (oldForums: ForumProps[] | undefined) => {
+          if (!oldForums) return [];
+          return oldForums.map((forum) =>
+            forum.id === forumId
+              ? { ...forum, totalLike: data.totalLike, isLiked: forum.isLiked }
+              : forum,
+          );
+        },
+      );
+    },
+    onError: (error, _forumId, context) => {
+      console.log("fail", error);
+
+      if (context?.previousData) {
+        queryClient.setQueryData(["fetchForum"], context.previousData);
+      }
+    },
+  });
+
+  if (isPending) {
+    return <LoadingPage />;
+  }
+
+  if (isError) {
+    return <ErrorPage />;
+  }
+
+  return (
+    <div className="container mx-auto flex max-w-7xl flex-col items-start p-4">
+      <h1 className="mb-8 w-full text-start text-3xl font-bold">
+        Flashcards Forum
+      </h1>
+      <div className="w-full max-w-4xl space-y-6">
+        {posts !== undefined && posts?.length > 0 ? (
+          posts.map((post) => (
+            <Card key={post.id} className="w-full">
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <div className="flex w-full items-center space-x-2">
+                  <div className="flex items-center space-x-2">
+                    <Avatar className="h-8 w-8 rounded-full">
+                      <AvatarImage
+                        src={post.user.image}
+                        alt={post.user.username}
+                      />
+                      <AvatarFallback className="rounded-full">
+                        {post.user.username.slice(0, 2).toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                    <CardTitle className="text-sm font-semibold">
+                      {post.user.username}
+                    </CardTitle>
+                  </div>
+                  <span className="text-xs text-muted-foreground">•</span>
+                  <small className="text-xs text-muted-foreground">
+                    {formatDistanceToNowStrict(new Date(post.createdAt))} ago
+                  </small>
+                </div>
+              </CardHeader>
+              <CardContent className="py-2">
+                <Link
+                  href={"#"}
+                  className="group flex items-start space-x-2 rounded-md p-2 transition-colors hover:bg-secondary"
+                >
+                  <div className="flex-grow">
+                    <div className="flex items-center justify-between">
+                      <h3 className="text-base font-medium transition-colors group-hover:text-primary">
+                        {post.title.replace(
+                          post.title.charAt(0),
+                          post.title.charAt(0).toUpperCase(),
+                        )}
+                      </h3>
+                      <ArrowUpRight className="h-4 w-4 text-muted-foreground transition-colors group-hover:text-primary" />
+                    </div>
+                    <p className="text-sm text-muted-foreground">
+                      {post.description}
+                    </p>
+                  </div>
+                </Link>
+              </CardContent>
+              <CardFooter className="flex justify-between py-2">
+                <div className="flex items-center space-x-2">
+                  <Button
+                    variant="ghost"
+                    size="sm"
+                    className="text-muted-foreground"
+                    onClick={() => likeMutation.mutate(post.id)}
+                    disabled={likeMutation.isPending}
+                  >
+                    {post.isLiked ? (
+                      <Heart
+                        className="mr-2 h-4 w-4"
+                        fill="#c20f10"
+                        color="#c20f10"
+                      />
+                    ) : (
+                      <Heart className="mr-2 h-4 w-4" />
+                    )}
+                    {post.totalLike}
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    size="sm"
+                    className="text-muted-foreground"
+                  >
+                    <MessageCircle className="mr-2 h-4 w-4" />
+                    {post.comments.length}
+                  </Button>
+                </div>
+              </CardFooter>
+              <Comment
+                forumId={post.id}
+                queryClient={queryClient}
+                key={post.id}
+              />
+            </Card>
+          ))
+        ) : (
+          <Empty
+            description="There are no posts available"
+            isActionButtonNeeded={false}
+            image={EmptyImage}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
