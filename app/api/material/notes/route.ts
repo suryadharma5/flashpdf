93f1b65cd290df3f501fabfc19e5515de0eb56cb
suryadharma5 <suryadharmasetiawan03@gmail.com>
2025-03-01 01:39:45 +0700

    chore: prettier fix

--- app/api/material/notes/route.ts
+++ app/api/material/notes/route.ts
@@ -1,14 +1,9 @@
 import { auth } from "@/auth";
-import { prismaClient } from "@/lib/db";
-import { loadDocumentIntoPineCone } from "@/lib/pinecone";
-import { saveNotes, getNotes, updateNotes } from "@/lib/repository/material/noteRepository";
 import {
-  createDocumentQuestion,
-  getAllDocuments,
-  getDocumentQuestion,
-} from "@/lib/repository/material/questionRepository";
-import { questionFormSchema, TDocumentSchema } from "@/lib/types/question-form";
-import { createQuestion } from "@/lib/util/openai-helper";
+  getNotes,
+  saveNotes,
+  updateNotes,
+} from "@/lib/repository/material/noteRepository";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
@@ -56,7 +51,6 @@ export async function POST(req: NextRequest) {
       status: 201,
     },
   );
-
 }
 
 export async function GET(req: NextRequest) {
@@ -94,42 +88,42 @@ export async function GET(req: NextRequest) {
       status: 200,
     },
   );
-
 }
 
-  export async function PUT(req: NextRequest) {
-    const body = await req.json();
-    const searchParams = req.nextUrl.searchParams;
-    const documentId = searchParams.get("documentId");
+export async function PUT(req: NextRequest) {
+  const body = await req.json();
+  const searchParams = req.nextUrl.searchParams;
+  const documentId = searchParams.get("documentId");
 
-    console.log("PUT request received", documentId, body);
-  
-    if (!documentId) {
-      return NextResponse.json(
-        { status: 400, message: "Bad Request" },
-        { status: 400 }
-      );
-    }
-  
-    const user = await auth();
-    const userId = user?.user.id;
-  
-    const updatedNotes = await updateNotes(documentId ?? "", userId ?? "", body.notes);
-  
-    if (!updatedNotes) {
-      return NextResponse.json(
-        { status: 500, message: "Internal Server Error" },
-        { status: 500 }
-      );
-    }
+  console.log("PUT request received", documentId, body);
 
-    console.log("updatedNotes", updatedNotes);
-  
+  if (!documentId) {
     return NextResponse.json(
-      { status: 200, message: "Updated", data: updatedNotes },
-      { status: 200 }
+      { status: 400, message: "Bad Request" },
+      { status: 400 },
     );
   }
 
-    
+  const user = await auth();
+  const userId = user?.user.id;
 
+  const updatedNotes = await updateNotes(
+    documentId ?? "",
+    userId ?? "",
+    body.notes,
+  );
+
+  if (!updatedNotes) {
+    return NextResponse.json(
+      { status: 500, message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+
+  console.log("updatedNotes", updatedNotes);
+
+  return NextResponse.json(
+    { status: 200, message: "Updated", data: updatedNotes },
+    { status: 200 },
+  );
+}
