ab2d12c178e9c74db246a0cfe64638beedd52094
suryadharma5 <suryadharmasetiawan03@gmail.com>
2025-03-01 01:42:06 +0700

    feat(save): add save API

---
+++ app/api/save/route.ts
@@ -0,0 +1,97 @@
+import { auth } from "@/auth";
+import {
+  deleteSavedDocument,
+  getSavedDocument,
+  getSavedDocumentsByUserId,
+  saveDocument,
+} from "@/components/save/saveRepository";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const documentId = searchParams.get("documentId");
+
+  if (documentId == null) {
+    return NextResponse.json(
+      {
+        status: 400,
+        message: "Invalid field type",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  const session = await auth();
+  const userId = session?.user.id;
+
+  const savedDocument = await getSavedDocument(userId, documentId);
+
+  if (!savedDocument) {
+    const documents = await saveDocument(userId, documentId);
+
+    if (!documents) {
+      return NextResponse.json(
+        {
+          status: 500,
+          message: "Internal Server Error",
+        },
+        {
+          status: 500,
+        },
+      );
+    }
+  } else {
+    const documents = await deleteSavedDocument(userId, documentId);
+
+    if (!documents) {
+      return NextResponse.json(
+        {
+          status: 500,
+          message: "Internal Server Error",
+        },
+        {
+          status: 500,
+        },
+      );
+    }
+  }
+
+  return NextResponse.json(
+    {
+      status: 200,
+      message: "OK",
+    },
+    {
+      status: 200,
+    },
+  );
+}
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const documentId = searchParams.get("documentId");
+
+  const session = await auth();
+  const userId = session?.user.id;
+
+  let data;
+
+  if (documentId) {
+    data = await getSavedDocument(userId, documentId);
+  } else {
+    data = await getSavedDocumentsByUserId(userId);
+  }
+
+  return NextResponse.json(
+    {
+      status: 200,
+      data: data,
+      message: "OK",
+    },
+    {
+      status: 200,
+    },
+  );
+}
---
+++ components/save/saveRepository.ts
@@ -0,0 +1,88 @@
+import { prismaClient } from "@/lib/db";
+
+export async function getSavedDocumentsByUserId(userId: string) {
+  const savedDocuments = await prismaClient.savedDocument.findMany({
+    where: {
+      userId,
+    },
+    select: {
+      documentId: true,
+      document: {
+        select: {
+          title: true,
+          user: {
+            select: {
+              username: true,
+            },
+          },
+          History: {
+            select: {
+              id: true,
+            },
+            where: {
+              userId,
+            },
+          },
+          _count: {
+            select: {
+              questions: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!savedDocuments) {
+    return null;
+  }
+
+  return savedDocuments;
+}
+
+export async function getSavedDocument(userId: string, documentId: string) {
+  const savedDocument = await prismaClient.savedDocument.findFirst({
+    where: {
+      userId,
+      documentId,
+    },
+  });
+
+  console.log({ savedDocument });
+
+  if (!savedDocument) {
+    return null;
+  }
+
+  return savedDocument;
+}
+
+export async function saveDocument(userId: string, documentId: string) {
+  const savedDocument = await prismaClient.savedDocument.create({
+    data: {
+      userId,
+      documentId,
+    },
+  });
+
+  if (!savedDocument) {
+    return null;
+  }
+
+  return savedDocument;
+}
+
+export async function deleteSavedDocument(userId: string, documentId: string) {
+  const savedDocument = await prismaClient.savedDocument.deleteMany({
+    where: {
+      userId,
+      documentId,
+    },
+  });
+
+  if (!savedDocument) {
+    return null;
+  }
+
+  return savedDocument;
+}
