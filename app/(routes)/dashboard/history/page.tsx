a630c320a607c58892588f46ba8ee4521d2e8b03
Ryan Reeves <unicornwhite88@gmail.com>
2025-03-09 16:43:13 +0700

    refactor(history) : delete progress data in history page

--- app/(routes)/dashboard/history/page.tsx
+++ app/(routes)/dashboard/history/page.tsx
@@ -88,51 +88,9 @@ export default function HistoryPage() {
 
   return (
     <div className="container mx-auto max-w-7xl p-4 sm:p-6">
-      <h1 className="mb-6 text-3xl font-bold">History</h1>
-
-      <div className="mb-6 grid gap-4 sm:grid-cols-3 sm:gap-6">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Average Grade</CardTitle>
-            <BarChart className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{averageGrade}%</div>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Total Tests Taken
-            </CardTitle>
-            <GraduationCap className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{totalTests}</div>
-            <p className="text-xs text-muted-foreground">
-              Pre-test{(pretestCount ?? 0) > 1 ? "s" : ""}: {pretestCount} |
-              Post-test{(posttestCount ?? 0) > 1 ? "s" : ""}: {posttestCount}
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Highest Score</CardTitle>
-            <Trophy className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">
-              {testHistory.length > 0
-                ? Math.max(...testHistory.map((test) => test.grade))
-                : "0.0"}
-              %
-            </div>
-          </CardContent>
-        </Card>
-      </div>
-
+      
       <div className="mb-6 flex flex-col items-start justify-between sm:flex-row sm:items-center">
-        <h2 className="mb-1 text-2xl font-semibold sm:mb-0">Test Details</h2>
+        <h2 className="mb-1 text-2xl font-semibold sm:mb-0">Test History</h2>
         {/* Search Test */}
       </div>
 
