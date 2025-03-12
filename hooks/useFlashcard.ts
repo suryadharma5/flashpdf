52ada8f30b7c7b86ea696743eb43e989737038fc
suryadharma5 <suryadharmasetiawan03@gmail.com>
2025-03-12 21:26:57 +0700

    feat(hook): add custom hook for retrieving flashcards keypoint & explanation

---
+++ hooks/useFlashcard.ts
@@ -0,0 +1,48 @@
+import { axiosInstance } from "@/lib/axios";
+import { useQuery } from "@tanstack/react-query";
+
+export type FlashCardDataProps = {
+  keyPoint: string;
+  explanation: string;
+};
+
+export const useFlashcard = (documentId: string) => {
+  const { data, isLoading, isError, isSuccess, error } = useQuery({
+    queryKey: ["fetchFlashcardData", documentId],
+    queryFn: async () => {
+      const res = await axiosInstance.get(
+        `/api/material?documentId=${documentId}&type=flashcard`,
+      );
+      return res.data;
+    },
+    enabled: !!documentId,
+  });
+
+  const flashcardsData: FlashCardDataProps[] =
+    data?.data.flashcards.map((f: FlashCardDataProps) => ({
+      keyPoint: f.keyPoint,
+      explanation: f.explanation,
+    })) || [];
+
+  let documentTitle = "";
+  let namespace = "";
+  let userId = "";
+
+  if (isSuccess) {
+    documentTitle = data.data.title;
+    namespace = data.data.namespace;
+    userId = data.data.user.id;
+  }
+
+  return {
+    documentTitle,
+    flashcardsData,
+    isLoading,
+    isError,
+    isSuccess,
+    error,
+    namespace,
+    documentId,
+    userId,
+  };
+};
