8755f6650ceb15e49948ce55586d8dcfa21ee643
suryadharma5 <suryadharmasetiawan03@gmail.com>
2025-04-04 15:54:01 +0800

    fix: cors issue

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        // Matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://app.flashai.site",
-          }, // Ganti dengan domain yang diizinkan
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const createNextIntlPlugin = require("next-intl/plugin");
